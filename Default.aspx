a7644fed03d2c441911e8f3eac4657fd0e56fafc
abcdlzy <abcdlzy@icloud.com>
2022-09-12 20:07:28 +0800

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="cloudcliper.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="tbtext" runat="server" Height="185px" TextMode="MultiLine" Width="986px"></asp:TextBox>
+            <br />
+            <asp:Button ID="btntextsubmit" runat="server" Text="保存文本" OnClick="btntextsubmit_Click" />
+&nbsp;
+            <asp:Label ID="lbTextSaveMsg" runat="server" Text="Label"></asp:Label>
+
+            <hr/>
+            <br />
+            <asp:Button ID="btnFileDownload" runat="server" OnClick="btnFileDownload_Click" Text="当前无文件" />
+            <br />
+            <br />
+            <asp:FileUpload ID="fufile" runat="server" />
+            <br />
+            <asp:Button ID="btnfilesubmit" runat="server" Text="保存文件" OnClick="btnfilesubmit_Click" />
+&nbsp;
+            <asp:Label ID="lbFileSaveMsg" runat="server" Text="Label"></asp:Label>
+
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cloudcliper
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public static string text = "";
+        public static string filename = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                tbtext.Text = text;
+
+            }
+            btnFileDownload.Text = filename;
+        }
+
+        protected void btntextsubmit_Click(object sender, EventArgs e)
+        {
+             text= tbtext.Text;
+            lbTextSaveMsg.Text = "保存时间"+DateTime.Now.ToString();
+        }
+
+        protected void btnfilesubmit_Click(object sender, EventArgs e)
+        {
+            string filepath = Server.MapPath("tempfile");
+            if (fufile.HasFile)
+            {
+                if (File.Exists(filepath))
+                {
+                    File.Delete(filepath);
+                }
+                fufile.SaveAs(filepath);
+                filename = fufile.FileName;
+                btnFileDownload.Text = filename;
+                lbFileSaveMsg.Text = "保存时间" + DateTime.Now.ToString();
+            }
+        }
+
+        protected void btnFileDownload_Click(object sender, EventArgs e)
+        {
+            string filepath = Server.MapPath("tempfile");
+            FileStream fs = new FileStream(filepath, FileMode.Open);
+            byte[] bytes = new byte[(int)fs.Length];
+            fs.Read(bytes, 0, bytes.Length);
+            fs.Close();
+            Response.ContentType = "application/octet-stream";
+            //通知浏览器下载文件而不是打开
+            Response.AddHeader("Content-Disposition", "attachment;  filename=" + HttpUtility.UrlEncode(filename, System.Text.Encoding.UTF8));
+            Response.BinaryWrite(bytes);
+            Response.Flush();
+            Response.End();
+
+
+
+        }
+    }
+}
